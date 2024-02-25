4bffd78f6018ea9a3afb262f8164b9aabb9992fd
mhdraslan <47928180+mhdraslan@users.noreply.github.com>
2024-02-25 04:52:11 +0400

    v3.6.1

---
+++ v3.6/Master/Cleanup.ps1
@@ -0,0 +1,14 @@
+$searchCriteria = "cnt*"
+
+$mgs = Get-AzManagementGroup
+foreach($mg in $mgs){
+    Get-AzManagementGroupDeployment -ManagementGroupId $mg.Name | Remove-AzManagementGroupDeployment -AsJob
+}
+
+
+$subs = Get-AzSubscription
+foreach($sub in $subs){
+    Select-AzSubscription $sub.Id
+    Get-AzSubscriptionDeployment | Remove-AzSubscriptionDeployment -AsJob 
+    Get-AzResourceGroup | ?{$_.ResourceGroupName -like $searchCriteria} | Remove-AzResourceGroup -AsJob -Force
+}
